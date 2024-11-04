3f6354ea84c343e830c25620ac5adfde55ef9cdd
jettberg <164446552+jettberg@users.noreply.github.com>
2024-11-04 13:20:22 -0600

    converted code into JQuery format

--- js/script.js
+++ js/script.js
@@ -1,48 +1,64 @@
 
-function newItem(){
-
-    //javascript
-    //1. Adding a new item to the list of items: 
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
-       }
-    
-     //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
-    
-         li.addEventListener("dblclick",crossOut);
-    
-     //3(i). Adding the delete button "X": 
-       let crossOutButton = document.createElement("crossOutButton");
-         crossOutButton.appendChild(document.createTextNode("X"));
-         li.appendChild(crossOutButton);
-    
-         crossOutButton.addEventListener("click", deleteListItem);
-     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-       function deleteListItem(){
-             li.classList.add("delete")
-         }
-     // 4. Reordering the items: 
-       $('#list').sortable();
-    
-    }
-    
+$(document).ready(function() {
      
+  function newItem() {
+
+let inputValue = $('input').val();
+if (inputValue === '') {
+  alert("You have to add something!");
+} else {
+  let li = $('<li></li>').text(inputValue);
+  $('#list').append(li);
+  $('input').val('');
+}
+
+li.on("dbclick", function() {
+  li.toggleClass("strike");
+});
+
+let crossOutButton = $('<button class= "crossOutButton">X</button>');
+li.append(crossOutButton);
+
+crossOutButton.on("click", function () {
+li.addClass("delete");
+});
+
+$('#list').append(li);
+$('#input').val('');
+}
+
+$('#button').on('click', newItem);
+    
+}); 
     
     
     
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     // If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
